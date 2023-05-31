429923824ecedab0fda7d1be8725976fac3ff35b
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-05-31 12:30:34 +0700

    update

---
+++ src/main2.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#define pin 13
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  // Display first O
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(pin, HIGH);
+    delay(500);
+    digitalWrite(pin, LOW);
+    delay(500);
+  }
+  // S
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(pin, HIGH);
+    delay(2000);
+    digitalWrite(pin, LOW);
+    delay(500);
+  }
+
+  for(int i = 0; i < 3; i++) {
+    digitalWrite(pin, HIGH);
+    delay(500);
+    digitalWrite(pin, LOW);
+    delay(500);
+  }
+  delay(5000);
+}
