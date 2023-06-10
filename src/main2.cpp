8aced2759b18e6e6a2d22457a33a806de761f760
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-06-10 09:39:33 +0700

    Update code and products

--- src/main2.cpp
+++ src/main2.cpp
@@ -1,34 +1,33 @@
 #include <Arduino.h>
-#define pin 13
+#include <math.h> // Other C++ libraries except STL can be included in adruino platform
 
+
+/*Create warning message when overloading an electrical device*/
+int redPin = 11;
+int readPin = A1;
+int readVal = 0;
+float Vout = 0.0;
+int rate = 9600;
+int delayVal = 500;
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(pin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(readPin, INPUT);
+  Serial.begin(rate);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
-  // Display first O
-  for(int i = 0; i < 3; i++) {
-    digitalWrite(pin, HIGH);
-    delay(500);
-    digitalWrite(pin, LOW);
-    delay(500);
-  }
-  // S
-  for(int i = 0; i < 3; i++) {
-    digitalWrite(pin, HIGH);
-    delay(2000);
-    digitalWrite(pin, LOW);
-    delay(500);
-  }
+  readVal = analogRead(readPin); // Read the value from the pin
 
-  for(int i = 0; i < 3; i++) {
-    digitalWrite(pin, HIGH);
-    delay(500);
-    digitalWrite(pin, LOW);
-    delay(500);
+  // Calculate the actual voltage
+  Vout = (5./1023.) * readVal;
+  Serial.println(Vout);
+  delay(delayVal);
+
+  // Alert the LED when the voltage above 3V
+  if(Vout > 3.0) {
+    analogWrite(redPin, 255);
   }
-  delay(5000);
+  else {
+    analogWrite(redPin, 0);
+  }
 }
